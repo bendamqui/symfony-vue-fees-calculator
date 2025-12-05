dfda5ee91c8d6849a2e41ad4940dbb6daa07f7df
bendamqui <bendamqui@icloud.com>
2025-12-05 10:17:01 -0500

    Minimal showIsFetching logic

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,8 +2,10 @@
   <main>
     <div class="fee-card">
       <h2>💰 Fee Calculator</h2>
+
       <FeeCalculatorForm @formReady="handleFormReady" />
       <div v-if="error" class="error">{{ error }}</div>
+      <div v-if="showIsFetching">Processing your request...</div>
       <FeeCalculatorBreakdown :result />
     </div>
   </main>
@@ -17,6 +19,8 @@ import type { FeeCalculatorPayload, FeeCalculatorResult } from '@/Types.ts'
 
 const result = ref<FeeCalculatorResult>()
 const error = ref('')
+const isFetching = ref(false)
+const showIsFetching = ref(false)
 
 const fetchFee = async (payload: FeeCalculatorPayload) => {
   const response = await fetch(
@@ -33,10 +37,20 @@ const fetchFee = async (payload: FeeCalculatorPayload) => {
 
 const handleFormReady = (payload: FeeCalculatorPayload) => {
   error.value = ''
+  isFetching.value = true
+  setTimeout(() => {
+    if (isFetching.value) {
+      showIsFetching.value = true
+    }
+  }, 1000)
   fetchFee(payload)
     .then((response) => (result.value = response))
     .catch(() => {
       error.value = 'Something went wrong'
+    })
+    .finally(() => {
+      isFetching.value = false
+      showIsFetching.value = false
     })
 }
 </script>
