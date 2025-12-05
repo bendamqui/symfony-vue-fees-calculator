9a5a2ed3ed806b69a90e41dbfb49410910283ae1
bendamqui <bendamqui@icloud.com>
2025-12-04 21:48:13 -0500

    Minimal fetch error handling

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,6 +3,7 @@
     <div class="fee-card">
       <h2>💰 Fee Calculator</h2>
       <FeeCalculatorForm @formReady="handleFormReady" />
+      <div v-if="error" class="error">{{ error }}</div>
       <FeeCalculatorBreakdown :result />
     </div>
   </main>
@@ -15,6 +16,7 @@ import { ref } from 'vue'
 import type { FeeCalculatorPayload, FeeCalculatorResult } from '@/Types.ts'
 
 const result = ref<FeeCalculatorResult>()
+const error = ref('')
 
 const fetchFee = async (payload: FeeCalculatorPayload) => {
   const response = await fetch(
@@ -23,9 +25,18 @@ const fetchFee = async (payload: FeeCalculatorPayload) => {
       headers: { 'Content-Type': 'application/json' },
     },
   )
-  return await response.json()
+  if (response.status !== 200) {
+    throw new Error()
+  }
+  return response.json()
 }
-const handleFormReady = async (payload: FeeCalculatorPayload) => {
-  result.value = await fetchFee(payload)
+
+const handleFormReady = (payload: FeeCalculatorPayload) => {
+  error.value = ''
+  fetchFee(payload)
+    .then((response) => (result.value = response))
+    .catch(() => {
+      error.value = 'Something went wrong'
+    })
 }
 </script>
